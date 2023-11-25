ba5525c824619b64ce053df458a649c90dadba52
zammyemprende <zammycred@gmail.com>
2023-11-25 11:05:26 +0100

    commit zammy

---
+++ aprendiendo_math.py
@@ -0,0 +1,4 @@
+import math
+
+numero = int(input('Ingresa un numero por favor'))
+print(f'Raiz de {numero} = {math.sqrt(numero)}')
---
+++ operadores.py
@@ -0,0 +1,9 @@
+"""
+OPERADPRES ARITMETICOS
+"""
+
+numero1 = int(input('Ingresa un numero:'))
+numero2 = int(input('Ingresa un numero:'))
+
+resultado = numero1 + numero2
+print(resultado)
